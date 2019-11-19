a86f0e6684d325a9338493b5d467b16f4c8005c7
Joggleston <johnathanbailey4@gmail.com>
2019-11-18 19:32:49 -0500

    Board without GUI or clicks

--- test.py
+++ test.py
@@ -3,14 +3,22 @@ from graphics import *
 win = GraphWin("test", 400, 400)
 class mySphere(object):
 
-    def __init__(mySphere, ring1, ring2):
-        ring1 = Circle(Point(mid1, mid2), span)
-        ring2 = Circle(Point(mid1, mid2), span/2)
-        
-    def draw(mySphere):
-        draw = mySphere.draw(win)
-        
-def create(mid1, mid2, span):
-    mySphere.draw(win)
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.r = 0
+        self.r2 = 0
+    def make():
+        self.make = self.draw(win)
+    
+def inner():
+    self.inner = Circle(Point(x, y), r)
+def outer():
+    self.outer = Circle(Point(x, y), r2)
 
-create(200, 200, 300)
+def create(x, y, r, r2):
+    inner = Circle(Point(x, y), r)
+    outer = Circle(Point(x, y), r2)
+    mySphere.make
+
+create(200, 200, 150, 300)
--- tiles.py
+++ tiles.py
@@ -20,24 +20,24 @@ if difficulty.lower()[0] == "e":
     x, y = 16, 30
     
 
-rect = Rectangle(Point(0, 0), Point(tileX, tileY-tileY/8))
+rect = Rectangle(Point(0, 0), Point(tileX-tileX/8, tileY-tileY/8))
 rect.setFill(color_rgb(225, 225, 225))
 rect.setOutline(color_rgb(225, 225, 225))
 
-bevelV = Rectangle(Point(tileX, tileY/8), Point(tileX+tileX/8, tileY-tileY/8))
+bevelV = Rectangle(Point(tileX-tileX/8, tileY/8), Point(tileX, tileY-tileY/8))
 bevelV.setFill(color_rgb(240, 240, 240))
 bevelV.setOutline(color_rgb(240, 240, 240))
 
-cornerV1 = Polygon(Point(tileX, 0), Point(tileX, tileY/8), Point(tileX+tileX/8, tileY/8))
+cornerV1 = Polygon(Point(tileX-tileX/8, 0), Point(tileX-tileX/8, tileY/8), Point(tileX, tileY/8))
 cornerV1.setFill(color_rgb(240, 240, 240))
 cornerV1.setOutline(color_rgb(240, 240, 240))
 
-cornerV2 = Polygon(Point(tileX, tileY-tileY/8), Point(tileX+tileX/8, tileY-tileY/8), Point(tileX+tileX/8, tileY))
+cornerV2 = Polygon(Point(tileX-tileX/8, tileY-tileY/8), Point(tileX, tileY-tileY/8), Point(tileX, tileY))
 cornerV2.setFill(color_rgb(240, 240, 240))
 cornerV2.setOutline(color_rgb(240, 240, 240))
 
 
-bevelH = Rectangle(Point(tileX/8, tileY-tileY/8), Point(tileX, tileY))
+bevelH = Rectangle(Point(tileX/8, tileY-tileY/8), Point(tileX-tileX/8, tileY))
 bevelH.setFill(color_rgb(175, 175, 175))
 bevelH.setOutline(color_rgb(175, 175, 175))
 
@@ -45,11 +45,11 @@ cornerH1 = Polygon(Point(0, tileY-tileY/8), Point(tileX/8, tileY-tileY/8), Point
 cornerH1.setFill(color_rgb(175, 175, 175))
 cornerH1.setOutline(color_rgb(175, 175, 175))
 
-cornerH2 = Polygon(Point(tileX, tileY-tileY/8), Point(tileX, tileY), Point(tileX+tileX/8, tileY))
+cornerH2 = Polygon(Point(tileX-tileX/8, tileY-tileY/8), Point(tileX-tileX/8, tileY), Point(tileX, tileY))
 cornerH2.setFill(color_rgb(175, 175, 175))
 cornerH2.setOutline(color_rgb(175, 175, 175))
 
-split = Line(Point(tileX, tileY-tileY/8), Point(tileX+tileX/8, tileY))
+split = Line(Point(tileX-tileX/8, tileY-tileY/8), Point(tileX, tileY))
 split.setOutline(color_rgb(150, 150, 150))
 
 rect.draw(win)
@@ -62,70 +62,76 @@ cornerH2.draw(win)
 split.draw(win)
 
 cloned = rect
+cloned2 = bevelV
+cloned3 = cornerV1
+cloned4 = cornerV2
+cloned5 = bevelH
+cloned6 = cornerH1
+cloned7 = cornerH2
+cloned8 = split
 
 for i in range(y):
     if i > 0:
-        trueX += tileX
-    for i in range(x):
+        cloned.move(tileY, -tileX*y)
+        cloned2.move(tileY, -tileX*y)
+        cloned3.move(tileY, -tileX*y)
+        cloned4.move(tileY, -tileX*y)
+        cloned5.move(tileY, -tileX*y)
+        cloned6.move(tileY, -tileX*y)
+        cloned7.move(tileY, -tileX*y)
+        cloned8.move(tileY, -tileX*y)
+    for j in range(x):
         if x == 0:
             cloned = rect
         cloned = cloned.clone()
         cloned.move(trueX, tileY)
         cloned.draw(win)
 
-    cloned = bevelV
-    for i in range(x):
+    for j in range(x):
         if x == 0:
-            cloned = bevelV
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
+            cloned2 = bevelV
+        cloned2 = cloned2.clone()
+        cloned2.move(trueX, tileY)
+        cloned2.draw(win)
 
-    cloned = cornerV1
-    for i in range(x):
+    for j in range(x):
         if x == 0:
-            cloned = cornerV1
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
+            cloned3 = cornerV1
+        cloned3 = cloned3.clone()
+        cloned3.move(trueX, tileY)
+        cloned3.draw(win)
 
-    cloned = cornerV2
-    for i in range(x):
+    for j in range(x):
         if x == 0:
-            cloned = cornerV2
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
+            cloned4 = cornerV2
+        cloned4 = cloned4.clone()
+        cloned4.move(trueX, tileY)
+        cloned4.draw(win)
 
-    cloned = bevelH
-    for i in range(x):
+    for j in range(x):
         if x == 0:
-            cloned = bevelH
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
+            cloned5 = bevelH
+        cloned5 = cloned5.clone()
+        cloned5.move(trueX, tileY)
+        cloned5.draw(win)
 
-    cloned = cornerH1
-    for i in range(x):
+    for j in range(x):
         if x == 0:
             cloned = cornerH1
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
+        cloned6 = cloned6.clone()
+        cloned6.move(trueX, tileY)
+        cloned6.draw(win)
 
-    cloned = cornerH2
-    for i in range(x):
+    for j in range(x):
         if x == 0:
-            cloned = cornerH2
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
+            cloned7 = cornerH2
+        cloned7 = cloned7.clone()
+        cloned7.move(trueX, tileY)
+        cloned7.draw(win)
 
-    cloned = split
-    for i in range(x):
+    for j in range(x):
         if x == 0:
-            cloned = split
-        cloned = cloned.clone()
-        cloned.move(trueX, tileY)
-        cloned.draw(win)
-    
+            cloned8 = split
+        cloned8 = cloned8.clone()
+        cloned8.move(trueX, tileY)
+        cloned8.draw(win)
